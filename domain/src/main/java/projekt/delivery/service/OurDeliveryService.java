96e7ff69a08b52fcc34e0382a81e82a85c428c41
Kristina Shigabutdinova <uneleeet@gmail.com>
2023-03-16 22:03:12 +0100

    H.9 BasicDeliveryService und OurDeliveryService soweit mit allen Methoden und Dokumentation (Kristina)

--- domain/src/main/java/projekt/delivery/service/OurDeliveryService.java
+++ domain/src/main/java/projekt/delivery/service/OurDeliveryService.java
@@ -7,6 +7,7 @@ import projekt.delivery.routing.Vehicle;
 import projekt.delivery.routing.VehicleManager;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
@@ -23,27 +24,44 @@ public class OurDeliveryService extends AbstractDeliveryService {
     }
     @Override
     protected List<Event> tick(long currentTick, List<ConfirmedOrder> newOrders) {
-            for (ConfirmedOrder order : newOrders) {
-                pendingOrders.add(order);
-            }
-            pendingOrders.sort(Comparator.comparing(ConfirmedOrder::getDeliveryTime));
-            for (ConfirmedOrder order : pendingOrders) {
-                if (order.getDeliveryTime() <= currentTime) {
-                    pendingOrders.remove(order);
+
+        vehicleManager.tick(currentTick);
+        List<Event> r = new ArrayList<>();
+
+        r.addAll(vehicleManager.tick(currentTick));
+
+        pendingOrders.addAll(newOrders);
+
+        pendingOrders.sort(Comparator.comparingLong(o -> o.getDeliveryInterval().start()));
+
+        //managing orders to restaurants
+        for (VehicleManager.OccupiedRestaurant restaurant : vehicleManager.getOccupiedRestaurants()) {
+            Collection<Vehicle> vehicles = restaurant.getVehicles();
+            for (Vehicle vehicle : vehicles) {
+                while (vehicle.getCapacity() > 0 && !pendingOrders.isEmpty()) {
+                    pendingOrders.remove(0);
                 }
             }
+
+
+            //moving to each point
             for (Vehicle vehicle : vehicleManager.getVehicles()) {
-                if (true) {
-                    for (ConfirmedOrder order : pendingOrders) {
-                        if (order.getDeliveryTime() <= currentTime) {
-                            pendingOrders.remove(order);
-                        }
-                    }
+                VehicleManager.OccupiedNeighborhood neighborhood = vehicleManager.getOccupiedNeighborhood(vehicle.getStartingNode().getComponent());
+                for(ConfirmedOrder order : newOrders) {
+
+                    neighborhood.deliverOrder(vehicle, order, currentTick);
+
+                }
+                if(vehicle.getOrders().isEmpty()){
+                    vehicle.moveQueued(neighborhood.getComponent());
                 }
             }
-            currentTime++;
-            return null;
-    } //TODO H9.2 - remove if implemented
+        }
+
+
+
+        return r;
+    }//TODO H9.2 - remove if implemented
     @Override
     public List<ConfirmedOrder> getPendingOrders() {
         return pendingOrders;
